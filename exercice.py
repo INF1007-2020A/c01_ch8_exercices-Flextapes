50b1e2066719b0b9bd56b51c5f408642c8b93ce6
carl salem <71038407+Flextapes@users.noreply.github.com>
2020-10-26 11:52:38 -0700

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,13 +3,29 @@
 
 
 # TODO: Importez vos modules ici
-
-
+import math
 
 # TODO: Définissez vos fonction ici
+def comparateur(name_file_1: str, name_file_2: str):
+    with open(name_file_1, "r") as f1, open(name_file_2, "r") as f2:
+        same = True
+
+        while same:
+            a = f1.read()
+            b = f2.read()
+
+            same = a == b
+    return same
+
+def triple_space(name_of_the_file: str):
+    with open(name_of_the_file, "r") as f3:
+        data = f3.read()
+        triple = data.replace(' ', '   ')
+        #et je sais plus comment on remplace :(
+                    
 
 
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
-
+    print(comparateur(fichier1, fichier2))
     pass
